69488f00bf9b7b013396c9320a7169a91f7abc3c
Muhammad Ata <muhammad_ata@outlook.com>
2022-05-20 21:28:31 +0500

    feat: create hourly card ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import ReactDom from 'react-dom';
 import CurrentTemp from "../components/CurrentStatus/CurrentTemp";
 import CurrentWeatherDetails from "../components/CurrentStatus/CurrentWeatherDetails";
 import Header from "../components/Header/Header";
+import HourlyCard from "../components/HourlyCard/HourlyCard";
 const App = () => {
     return (
         <div className="main-container">
             <Header />
             <CurrentTemp />
             <CurrentWeatherDetails />
+            <HourlyCard />
         </div>
     );
 }
