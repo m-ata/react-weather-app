f58e72344d2354dfdd5e90edba139acf9e4b074c
Muhammad Ata <muhammad_ata@outlook.com>
2022-05-28 01:13:11 +0500

    feat: add background image in currenttemp component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import DailyForecast from "./components/DailyForecast/DailyForecast";
 import Header from "./components/Header/Header";
 import HourlyCards from "./components/HourlyCards/HourlyCards";
 import { useWeather } from "./hooks/useWeather";
+import WeatherBackgroundImages from "./utils/backgroundImages.json";
 const App = () => {
     const WeatherData = useWeather();
     return (
         <div className="main-container">
             <Header />
-            <CurrentTemp currentWeather={WeatherData} />
+            <CurrentTemp currentWeather={WeatherData} WeatherImages = {WeatherBackgroundImages || []} />
             <HourlyCards forecastDay= {WeatherData?.forecast?.forecastday || []} />
             <DailyForecast forecastDay = {WeatherData?.forecast?.forecastday || []} />
         </div>
--- src/components/CurrentStatus/CurrentTemp.jsx
+++ src/components/CurrentStatus/CurrentTemp.jsx
@@ -1,28 +1,56 @@
-import React from 'react';
-import CurrentWeatherDetails from './CurrentWeatherDetails';
-import './style.scss';
-const CurrentTemp = ( {currentWeather} ) => {
-    const {location, current} = currentWeather;
-    return (
-    <div className='container'>
-        <div className='currentStatus'>
-            <div className='currentStatus__location'>
-                <h1 className='currentStatus__location__title'>{location?.name}</h1>
-                <h3 className='currentStatus__location__name'>{ location?.country }</h3>
-                <span className='currentStatus__location__date'>{ current?.last_updated }</span>
-            </div>
-            <div className='currentStatus__temperature'>
-                {/* <span className='currentStatus__temperature__icon'> ☀️ </span> */}
-                <img src={current?.condition?.icon} alt="icon" className='currentStatus__temperature__icon'/>
-                <span className='currentStatus__temperature__details'>
-                    <h2 className='currentStatus__temperature__details__temp'>{current?.temp_f}°<span> F</span></h2>
-                    <span className='currentStatus__temperature__details__feelLike'>Feel Like {current?.feelslike_f}° </span>
-                    <span className='currentStatus__temperature__details__description'>{current?.condition?.text} sky</span>
-                </span>
-            </div>
+import React from "react";
+import CurrentWeatherDetails from "./CurrentWeatherDetails";
+import "./style.scss";
+const CurrentTemp = ({ currentWeather, WeatherImages }) => {
+  const { location, current } = currentWeather;
+
+
+  console.log(current);
+  const getWeatherBackground = () => {
+    const background = WeatherImages.find(
+      (item) => item?.code === current?.condition?.code
+    );
+    return background?.background;
+  };
+  return (
+    <div style={{backgroundImage: `url( ${getWeatherBackground()})`}} className="weatherBackground">
+      <div className="container">
+        <div className="currentStatus">
+          <div className="currentStatus__location">
+            <h1 className="currentStatus__location__title">{location?.name}</h1>
+            <h3 className="currentStatus__location__name">
+              {location?.country}
+            </h3>
+            <span className="currentStatus__location__date">
+              {current?.last_updated}
+            </span>
+          </div>
+          <div className="currentStatus__temperature">
+            {/* <span className='currentStatus__temperature__icon'> ☀️ </span> */}
+            <img
+              src={current?.condition?.icon}
+              alt="icon"
+              className="currentStatus__temperature__icon"
+            />
+            <span className="currentStatus__temperature__details">
+              <h2 className="currentStatus__temperature__details__temp">
+                {current?.temp_f}°<span> F</span>
+              </h2>
+              <span className="currentStatus__temperature__details__feelLike">
+                Feel Like {current?.feelslike_f}°{" "}
+              </span>
+              <span className="currentStatus__temperature__details__description">
+                {current?.condition?.text} sky
+              </span>
+            </span>
+          </div>
         </div>
-        <CurrentWeatherDetails CurrentWeatherDetails = {current} locationName={location?.name} />
+        <CurrentWeatherDetails
+          CurrentWeatherDetails={current}
+          locationName={location?.name}
+        />
+      </div>
     </div>
-     );
-}
+  );
+};
 export default CurrentTemp;
