99a533cc89dbe98e33005148aebef5d5c5d50e2f
Muhammad Ata <muhammad_ata@outlook.com>
2022-06-01 03:24:59 +0500

    feat: setupl formatjs and addded turkish lang json file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
+import { IntlProvider, FormattedMessage } from "react-intl";
 import { Provider as ReduxProvider } from "react-redux";
 import { store } from "./redux/store";
 import AppRouter from "./Router";
-
+import messagesInTurkish from "./lang/tr.json"
 const App = () => {
   return (
     <ReduxProvider store={store}>
-      <AppRouter />
+      <IntlProvider messages={messagesInTurkish} locale="tr" defaultLocale="en">
+          <AppRouter />
+      </IntlProvider>
     </ReduxProvider>
   );
 };
